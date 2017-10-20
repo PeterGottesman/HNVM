c5c3b5afa4f08e8cf82cca52dbf437f42d369d39
Peter Gottesman <peter@petergottesman.com>
2017-10-20 11:04:08 -0400

    Fixed memory allocation, changed debug output

--- src/vm.c
+++ src/vm.c
@@ -16,29 +16,23 @@ int initialize_vm(vm_type *vm)
 
 int allocate_memory(vm_type *vm)
 {
-    vm->memory = calloc(sizeof(uint8_t), conf.memory);
+    vm->memory = &vm->registers + sizeof(vm->registers);
     if (vm->memory == NULL) return 1;
     return 0;
 }
 
 int loop(vm_type *vm)
 {
-    uint8_t prog[] = {LDM, 12, 100, LDM, 13, 34, ADD, 12, SUB, 13, HCF};
-    for(int i = 0; i < 11; i++)
+    uint8_t prog[] = {LDM, 32, 100, LDM, 13, 34, ADD, 12, SUB, 13, HCF};
+    for(int i = 0; i < sizeof(prog); i++)
     {
 	vm->memory[i] = prog[i];
+	printf("%#04x ", vm->memory[i]);
     }
+    printf("\n");
     vm->running=1;
     while(vm->running)
     {
-	/*
-        for (int i = 0; i < conf.memory; i++)
-        {
-	    printf("%d: %d\n", i, vm->memory[i]);
-        }
-	printf("PC: %d, %d\n", vm->PC, vm->memory[vm->PC]);
-	printf("regA: %d\n", vm->registers.regA);
-	*/
 	eval_command(vm, vm->memory[vm->PC++%conf.memory]);
     }
     return 0;
@@ -49,43 +43,66 @@ int eval_command(vm_type *vm, VM_Instruction instr)
     switch(instr)
     {
         case NOP:
+	    #ifdef DEBUG
 	    printf("nop\n");
+	    #endif
 	    break;
 	case ADD:
+	    #ifdef DEBUG
 	    printf("add\n");
+	    #endif
 	    vm_add(vm, vm->memory[vm->PC++]);
 	    break;
 	case SUB:
+	    #ifdef DEBUG
 	    printf("sub\n");
+	    #endif
 	    vm_sub(vm, vm->memory[vm->PC++]);
 	    break;
 	case JEZ:
+	    #ifdef DEBUG
 	    printf("jez\n");
+	    #endif
 	    vm_jez(vm, vm->memory[vm->PC++], vm->PC++);
 	    break;
 	case JNZ:
+	    #ifdef DEBUG
 	    printf("jnz\n");
+	    #endif
 	    vm_jnz(vm, vm->memory[vm->PC++], vm->PC++);
 	    break;
         case LDA:
+	    #ifdef DEBUG
 	    printf("lda\n");
+	    #endif
 	    vm_lda(vm, vm->memory[vm->PC++]);
 	    break;
         case LDB:
+	    #ifdef DEBUG
 	    printf("ldb\n");
+	    #endif
 	    vm_lda(vm, vm->memory[vm->PC++]);
 	    break;
         case LDC:
+	    #ifdef DEBUG
 	    printf("ldc\n");
+	    #endif
 	    vm_lda(vm, vm->memory[vm->PC++]);
 	    break;
         case LDM:
+	    #ifdef DEBUG
 	    printf("ldm\n");
+	    #endif
             vm_ldm(vm, vm->memory[vm->PC++], vm->memory[vm->PC++]);
 	    break;
 	case HCF:
+	    #ifdef DEBUG
 	    printf("hcf\n");
+	    #endif
 	    vm->running = 0;
+	    break;
+        default:
+	    printf("unknown instruction %#04x at PC: %d\n", instr, vm->PC);
 	    break;
     }
     return 0;
@@ -141,7 +158,6 @@ int vm_ldc(vm_type *vm, uint8_t val)
 
 int vm_ldm(vm_type *vm, uint8_t dest, uint8_t val)
 {
-    printf("dest: %d, val: %d\n", dest, val);
     vm->memory[dest] = val;
     return 0;
 }
